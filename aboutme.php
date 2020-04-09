287c0dfddb7ef4b5b2ef7c0f667d5ee2051c70d9
Abdi maualana <63388851+AbdiMaulana1802@users.noreply.github.com>
2020-04-09 14:12:07 +0700

    menambahkan no hp dan Email dan menghapus alamat
    Update menambahkan no hp dan Email dan menghapus alamat

---
+++ aboutme.php
@@ -0,0 +1,8 @@
+<?php
+echo '<br>';
+echo 'nama  : Abdi Maulana <br>';
+echo 'kelas : XI RPL 1 <br>';
+echo 'asal sekolah : SMK Taruna Bhakti <br>';
+echo 'No HP : 089638349246 <br>';
+echo 'Email : abdimaulana200318@gmail.com <br>';
+?>
